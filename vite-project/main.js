a234b7db9076f021c967440fd02cfc457e0186fb
nathannathan08 <nathanc245@nycstudents.net>
2024-10-30 13:55:04 -0400

    fuklujmwuyfl;jmjmlsmn kmvfnjmbgkljm

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,24 +1,15 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "/style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const DOMselectors = {
+    button: document.querySelector(".btn"),
+};
 
-setupCounter(document.querySelector('#counter'))
+DOMselectors.button.addEventListener("click", function(event) {
+    event.preventDefault();
+    
+    // Toggle theme classes
+    document.body.classList.toggle("warm");
+    document.body.classList.toggle("cool");
+
+    console.log("Theme changed!");
+});
