3de4c20a8fce6b9389220661846cf18f9ea1eb80
nathannathan08 <nathanc245@nycstudents.net>
2024-11-12 13:52:31 -0500

    6yfivpot7

---
+++ vite-project/src/products.js
@@ -0,0 +1,23 @@
+
+export const cardData = [
+    { header: 'ewwww', imgSrc: 'brothaeugh.jpg', price: 'smelly', category: 'B' },
+    { header: 'GADZOOKS', imgSrc: 'WHATINTHE.jpg', price: 'GEE WILLICKERS', category: 'A' },
+    { header: 'el', imgSrc: 'elgato.webp', price: 'el gato', category: 'B' },
+    { header: 'this one', imgSrc: 'vexed.jpg', price: 'this one mad', category: 'B' },
+    { header: 'cat', imgSrc: 'smudge.jpg', price: 'D:', category: 'B' },
+    { header: 'ayo', imgSrc: 'sideeye.jpg', price: 'bro?', category: 'A' },
+    { header: 'eugh', imgSrc: 'disheveledlittleguy.jpg', price: 'man', category: 'B' },
+    { header: 'huh', imgSrc: 'huh.jpg', price: '?', category: 'A' },
+    { header: 'JUMPIN JAHOSAPHATS', imgSrc: 'WHATISTHIS.jpg', price: 'WOWZERS', category: 'A' },
+    { header: 'die', imgSrc: 'catpawnch.jpg', price: 'now', category: 'B' },
+    { header: 'JESUS', imgSrc: 'bro.jpg', price: 'GOD', category: 'B' },
+    { header: 'going to', imgSrc: 'imgoingtoharmyou.jpg', price: 'hurt you severely', category: 'B' },
+    { header: 'isnt this', imgSrc: 'teehee.jpg', price: 'the same dog as the shiver me timbers one', category: 'A' },
+    { header: 'look at this thing', imgSrc: 'manwhat.jpg', price: 'i dont even have anything funny to add to this what is this', category: 'A' },
+    { header: 'honk', imgSrc: 'honkshoo.jpg', price: 'mimimimimi', category: 'B' },
+    { header: ':3', imgSrc: 'colonthree.jpg', price: ':3', category: 'B' },
+    { header: 'huh', imgSrc: 'excuseme.jpg', price: 'what', category: 'A' },
+    { header: 'sephiroth has nothing on this guy', imgSrc: 'finalboss.jpg', price: 'bottom text', category: 'A' },
+    { header: ':0', imgSrc: 'gadzooks.jpg', price: ':0', category: 'B' },
+    { header: 'mad', imgSrc: 'angry.jpg', price: 'very angry', category: 'B' }
+  ];
